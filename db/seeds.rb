be4e610947df06406469a85c9e4212d885fc0027
Jen Trudell <jtrudell@gmail.com>
2016-01-24 16:42:46 -0600

    update seed file to include accurate picks

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,8 +35,112 @@ users = User.create([
   {name: 'Kevin B'}
   ])
 
-users.each do |user|
-  5.times do
-    user.picks.create(team_id: rand(0..Team.count))
-  end
-end
+Pick.create([
+  {user_id: 1, team_id: 17}, #texas
+  {user_id: 1, team_id: 40}, #indiana
+  {user_id: 1, team_id: 7},
+  {user_id: 1, team_id: 74}, #georgetown
+  {user_id: 1, team_id: 21}, #michigan state
+
+  {user_id: 2, team_id: 33},
+  {user_id: 2, team_id: 39},
+  {user_id: 2, team_id: 7},
+  {user_id: 2, team_id: 3},
+  {user_id: 2, team_id: 9}, #maryland
+
+  {user_id: 3, team_id: 3}, #kansas
+  {user_id: 3, team_id: 7},
+  {user_id: 3, team_id: 90}, #lsu
+  {user_id: 3, team_id: 1}, #oklahoma
+  {user_id: 3, team_id: 33},
+
+  {user_id: 4, team_id: 33},
+  {user_id: 4, team_id: 9}, #maryland
+  {user_id: 4, team_id: 7},
+  {user_id: 4, team_id: 39}, #california
+  {user_id: 4, team_id: 194}, #old dominion
+
+  {user_id: 5, team_id: 79}, #ohio state
+  {user_id: 5, team_id: 33}, #michigan
+  {user_id: 5, team_id: 42}, #witchita st
+  {user_id: 5, team_id: 7}, #north carolina
+  {user_id: 5, team_id: 34}, #pittsburgh
+
+  {user_id: 6, team_id: 9},
+  {user_id: 6, team_id: 21},
+  {user_id: 6, team_id: 3},
+  {user_id: 6, team_id: 7},
+  {user_id: 6, team_id: 179}, #ball state
+
+  {user_id: 7, team_id: 9},
+  {user_id: 7, team_id: 90},
+  {user_id: 7, team_id: 7},
+  {user_id: 7, team_id: 39},
+  {user_id: 7, team_id: 58}, #UConn
+
+  {user_id: 8, team_id: 9},
+  {user_id: 8, team_id: 21},
+  {user_id: 8, team_id: 7},
+  {user_id: 8, team_id: 39},
+  {user_id: 8, team_id: 33},
+
+  {user_id: 9, team_id: 3},
+  {user_id: 9, team_id: 9},
+  {user_id: 9, team_id: 7},
+  {user_id: 9, team_id: 102}, #UAB
+  {user_id: 9, team_id: 128}, #Marquette
+
+  {user_id: 10, team_id: 7},
+  {user_id: 10, team_id: 1},
+  {user_id: 10, team_id: 33},
+  {user_id: 10, team_id: 21},
+  {user_id: 10, team_id: 58},
+
+  {user_id: 11, team_id: 42},
+  {user_id: 11, team_id: 9},
+  {user_id: 11, team_id: 13}, #iowa st
+  {user_id: 11, team_id: 48}, #butler
+  {user_id: 11, team_id: 163}, #missouri
+
+  {user_id: 12, team_id: 17},
+  {user_id: 12, team_id: 7},
+  {user_id: 12, team_id: 39},
+  {user_id: 12, team_id: 33},
+  {user_id: 12, team_id: 28}, #duke
+
+  {user_id: 13, team_id: 39}, #california
+  {user_id: 13, team_id: 26}, #baylor
+  {user_id: 13, team_id: 3}, #kansas
+  {user_id: 13, team_id: 12}, #virginia
+  {user_id: 13, team_id: 33},
+
+  {user_id: 14, team_id: 7},
+  {user_id: 14, team_id: 3},
+  {user_id: 14, team_id: 1},
+  {user_id: 14, team_id: 39},
+  {user_id: 14 , team_id: 40},
+
+  {user_id: 15, team_id: 40},
+  {user_id: 15, team_id: 3},
+  {user_id: 15, team_id: 7},
+  {user_id: 15, team_id: 58},
+  {user_id: 15, team_id: 39},
+
+  {user_id: 16, team_id: 9},
+  {user_id: 16, team_id: 35}, #valpo
+  {user_id: 16, team_id: 3},
+  {user_id: 16, team_id: 7},
+  {user_id: 16, team_id: 177}, #tenn state
+
+  {user_id: 17, team_id: 3},
+  {user_id: 17, team_id: 84}, #tenn
+  {user_id: 17, team_id: 161}, #harvard
+  {user_id: 17, team_id: 42},
+  {user_id: 17, team_id: 17},
+
+  {user_id: 18, team_id: 7},
+  {user_id: 18, team_id: 9},
+  {user_id: 18, team_id: 8}, #texas a&m
+  {user_id: 18, team_id: 90},
+  {user_id: 18, team_id: 39}
+  ])

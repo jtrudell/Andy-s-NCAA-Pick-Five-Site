166de74747b9d1a61e6e833c6263e6883b426d26
Jen Trudell <jtrudell@gmail.com>
2016-01-24 19:24:36 -0600

    update favicon, remove comments from js

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('.collapsible').collapsible({
-      accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      accordion : true
     });
   });

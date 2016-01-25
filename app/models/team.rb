11404b5b28b3b66a34dbc40e766749f0e5b5987f
Jen Trudell <jtrudell@gmail.com>
2016-01-24 19:55:12 -0600

    add validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,7 @@ require 'ncaa_scrape'
 class Team < ActiveRecord::Base
   has_many :picks
   has_many :users, through: :picks
+  validates :name, presence: true
 
   def self.update_wins
     scrape = NCAABasketball.new

7cf022b2a01b70ec806d59ce349ea7874d6c7c2e
Jen <jtrudell@gmail.com>
2016-10-09 10:15:46 -0500

    revert to heroku branch

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-### NCAA Pick Five
+## NCAA Pick Five
 
 In progress. Sinatra app using Ruby and JS.
+
+### Getting Started
+0. bundle install
+1. rake db:create
+2. rake db:migrate
+3. rake db:seed
+4. shotgun config.ru
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,27 @@
+'use strict';
+
+var Headline = React.createClass({
+  displayName: 'Headline',
+
+  render: function render() {
+    // to come: startYear will be generated when new season begins
+    var startYear = 2015;
+    var endYear = startYear + 1;
+
+    return React.createElement(
+      'div',
+      {className: 'headline center'},
+      React.DOM.h1(null, 'NCAA Pick Five Challenge'),
+      React.DOM.h2(null, startYear + '/' + endYear )
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(Headline, null),
+  document.querySelector('.headline')
+);
+
 $(document).ready(function(){
     $('.collapsible').collapsible({
       accordion : true

c48b3662a1108bf733d83a78ffe209ee9ff5fe23
Jen <jtrudell@gmail.com>
2016-10-15 11:57:26 -0500

    generate year dynamically passed on bath

--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,42 @@
 'use strict';
 
+// title
+var yearParam = window.location.pathname.split('/').pop();
+var startYear = function() {
+  if (yearParam.length !== 4) {
+    return new Date().getFullYear();
+  }
+  return parseInt(yearParam)
+}
+
 var Headline = React.createClass({
   displayName: 'Headline',
 
   render: function render() {
-    // to come: startYear will be generated when new season begins
-    var startYear = 2016;
-    var endYear = startYear + 1;
+    var endYear = startYear() + 1;
 
     return React.createElement(
       'div',
       {className: 'headline center'},
       React.DOM.h1(null, 'NCAA Pick Five Challenge'),
-      React.DOM.h2(null, startYear + '/' + endYear )
+      React.DOM.h2(null, startYear() + '/' + endYear )
     );
   }
 });
+
+// this is completely unecessary and for experimentation
+Headline.defaultProps = {
+  startYear: startYear()
+};
 
 ReactDOM.render(
   React.createElement(Headline, null),
   document.querySelector('.headline')
 );
 
+// materialize
 $(document).ready(function(){
-    $('.collapsible').collapsible({
-      accordion : true
-    });
+  $('.collapsible').collapsible({
+    accordion : true
   });
+});

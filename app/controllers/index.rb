b4c7aecf6d3e0b135120890c3883c549a72e9f5e
Jen <jtrudell@gmail.com>
2016-10-16 16:51:10 -0500

    update ActiveRecord methods; reactor #update_wins

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,39 +4,39 @@ end
 
 before '/2016' do
   Team.update_wins(2016)
-  @users = User.where(year: 2016).sort { |x, y| x.name <=> y.name }
+  @users = User.where(year: 2016).order('name')
   @standings = User.standings(2016)
 end
 
 get '/:year' do
   year = params[:year].to_i
-  @users = User.where(year: year).sort { |x, y| x.name <=> y.name }
+  @users = User.where(year: year).order('name')
   @standings = User.standings(year)
   erb :"index"
 end
 
 get '/picks/:year' do
-  @year = params[:year].to_i
-  redirect '/' if Time.now.year != @year
-  @team_list = Team.two_columns(@year)
+  year = params[:year].to_i
+  redirect '/' if Time.now.year != year
+  @team_list = Team.two_columns(year)
   erb :"signup"
 end
 
 post '/picks/:year' do
-  @year = params[:year].to_i
-  redirect '/' if Time.now.year != @year
-  @user = User.new(name: params['name'], year: @year)
+  year = params[:year].to_i
+  redirect '/' if Time.now.year != year
+  @user = User.new(name: params['name'], year: year)
   teams = params['teams']
   if teams.nil? || teams.count != 5
     flash[:notice] = "You selected #{teams.count} teams: #{teams.keys}. You must pick 5 teams."
-    redirect "/picks/#{@year}"
+    redirect "/picks/#{year}"
   end
   @picks = Pick.generate(@user, teams)
   if @user.save
-    redirect "/users/#{@user.id}/picks/#{@year}"
+    redirect "/users/#{@user.id}/picks/#{year}"
   else
     flash[:notice] = "User name #{params['name']} exists. Try another name."
-    redirect "/picks/#{@year}"
+    redirect "/picks/#{year}"
   end
 end
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -7,22 +7,21 @@ class Team < ActiveRecord::Base
   validates :name, presence: true
   validates_uniqueness_of :name, scope: :year
 
-  attr_reader :name
-
   def self.update_wins(year)
     teams = Team.where(year: year)
-    if Time.now < Time.new(year, 11)
+    current_year = Time.now.year
+    if year == current_year && Time.now < Time.new(year, 11)
       teams.each { |team| team.update(wins: 0) }
-    elsif Time.now.year > year + 1
-      return
-    else
+    elsif year == current_year || year == current_year - 1
       scrape = NCAABasketball.new
       teams.each { |team| team.update(wins: scrape.team_wins(team.name)) }
+    else
+      teams
     end
   end
 
   def self.two_columns(year)
-    teams = Team.where(year: year).order(:name)
+    teams = Team.where(year: year).order('name')
     {
       first_half: teams[0...teams.count/2],
       second_half: teams[teams.count/2..-1]

cee63fe61504f2e2c8403d36b63d06be7ba946c5
Jen <jtrudell@gmail.com>
2016-11-13 14:17:17 -0600

    temp fix for wins until NCAA updates site

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates_uniqueness_of :name, scope: :year, message: 'That name has already been taken.'
 
-  def wins
-    self.teams.inject(0) { |total, team| total + team.wins }
+  def wins(year)
+    self.teams.where(year: year).inject(0) { |total, team| total + team.wins }
   end
 
   def self.standings(year)
     @users = User.where(year: year)
-    @users.sort { |x, y| y.wins <=> x.wins }
+    @users.sort { |x, y| y.wins(year) <=> x.wins(year) }
   end
 end

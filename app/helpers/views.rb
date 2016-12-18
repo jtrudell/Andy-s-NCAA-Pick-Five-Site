5b845f8b15b5f13e12df69fade5dcbd3d21f7729
Jen <jtrudell@gmail.com>
2016-12-18 07:13:21 -0600

    adds more view helpers

--- app/helpers/views.rb
+++ app/helpers/views.rb
@@ -1,5 +1,23 @@
 helpers do
-  def display_user(user, year)
-    "#{user.name} (#{year - 1} total: #{Pick.previous_season_total_wins(user.teams, year)})"
+  def display_user(user)
+    if @year == 2015
+      "#{user.name} | #{user.wins(@year) || 0} wins"
+    else
+      "#{user.name} (#{@year - 1} total: #{Pick.previous_season_total_wins(user.teams, @year)})"
+    end
+  end
+
+  def display_team(user, team)
+    if @year == 2015
+      "#{user.teams.index(team) + 1}. #{team.name} | 2015/2016 Wins: #{team.wins}"
+    else
+      "#{user.teams.index(team) + 1}. #{team.name} | Wins:\
+        #{@current_year_teams.find_or_create_by(name: team.name, year: @year).wins}\
+        (Last Year: #{@previous_year_teams.find_or_create_by(name: team.name, year: @year - 1).wins})"
+    end
+  end
+
+  def display_standings(user)
+    "#{@standings.index(user) + 1}. #{user.name} (wins: #{user.wins(@year)})"
   end
 end

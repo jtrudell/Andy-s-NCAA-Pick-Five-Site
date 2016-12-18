c228afed69f7a4a2acbe2f3257415d519ab89b43
Jen <jtrudell@gmail.com>
2016-12-18 07:58:35 -0600

    additional view helpers

--- app/helpers/views.rb
+++ app/helpers/views.rb
@@ -1,24 +1,32 @@
 helpers do
+  def wins(user, year = @year)
+    "#{user.wins(year)}"
+  end
+
+  def last_year(year = @year)
+    year - 1
+  end
+
   # 2015 was first year; no 'previous_season/_year" for 2015
   def display_user(user)
     if @year == 2015
-      "#{user.name} | #{user.wins(@year) || 0} wins"
+      "#{user.name} | #{wins(user) || 0} wins"
     else
-      "#{user.name} (#{@year - 1} total: #{Pick.previous_season_total_wins(user.teams, @year)})"
+      "#{user.name} (#{last_year} total: #{Pick.previous_season_total_wins(user.teams, @year)})"
     end
   end
 
-  def display_team(user, team)
+  def display_team(user, team, index)
     if @year == 2015
-      "#{user.teams.index(team) + 1}. #{team.name} | 2015/2016 Wins: #{team.wins}"
+      "#{index + 1}. #{team.name} | 2015/2016 Wins: #{team.wins}"
     else
-      "#{user.teams.index(team) + 1}. #{team.name} | Wins:\
+      "#{index + 1}. #{team.name} | Wins:\
         #{@current_year_teams.find_or_create_by(name: team.name, year: @year).wins}\
-        (Last Year: #{@previous_year_teams.find_or_create_by(name: team.name, year: @year - 1).wins})"
+        (Last Year: #{@previous_year_teams.find_or_create_by(name: team.name, year: last_year).wins})"
     end
   end
 
-  def display_standings(user)
-    "#{@standings.index(user) + 1}. #{user.name} (wins: #{user.wins(@year)})"
+  def display_standings(user, index)
+    "#{index + 1}. #{user.name} (wins: #{wins(user)})"
   end
 end
